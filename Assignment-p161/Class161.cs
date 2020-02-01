a7881e507c6eb68d10572f2add5f2162d406423f
Felix Ferreiras <a.ferreiras@outlook.com>
2020-02-01 17:16:11 -0500

    New

---
+++ Assignment-p161/Class161.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assignment_p161
+{
+    public class Class161
+    {
+        public void Method161(int x, int y)
+        {
+            x++;
+            Console.WriteLine("first parameter: " + x);
+            Console.WriteLine("Second parameter: " + y);
+        }
+    }
+}
---
+++ Assignment-p161/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assignment_p161
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new Class161();
+            obj.Method161(3, 3);
+            Console.ReadLine();
+        }
+    }
+}
