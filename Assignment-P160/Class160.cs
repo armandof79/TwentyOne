c5fe66b2d67e4d30399900041e297dd0ec228579
Felix Ferreiras <a.ferreiras@outlook.com>
2020-02-01 14:34:56 -0500

    ghj

---
+++ Assignment-P160/Class160.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assignment_P160
+{
+    public class Class160
+    {
+        public int Method1(int num, int num2 = 1)
+        {            
+            int result = (num * 2) * num2;
+            return result;
+        }
+    }
+}
---
+++ Assignment-P160/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assignment_P160
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an integer");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter an optional second integer");
+            int b = Convert.ToInt32(Console.ReadLine());
+            var obj = new Class160();
+            Console.WriteLine(obj.Method1(a, b));
+          
+            Console.ReadLine();
+
+        }
+    }
+}
