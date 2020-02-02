591f9ca999015543f4f7a3045120ad44e841d0da
Felix Ferreiras <a.ferreiras@outlook.com>
2020-02-02 17:10:17 -0500

    New Assignment

---
+++ Assignment-p176/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assignment_p176
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new Employee();
+            obj.firstName = "Sample";
+            obj.lastName = "Student";
+            obj.SayName();
+            Console.ReadLine();
+        }
+    }
+}
